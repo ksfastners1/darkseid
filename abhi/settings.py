237fa70aa412556048f90efe1fabad1609f2ac64
ksfastners1 <74371662+ksfastners1@users.noreply.github.com>
2020-11-13 12:53:34 +0530

    Update settings.py

--- abhi/settings.py
+++ abhi/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'p)_vql_+!q2p_xroibdwnoka8w9)&khr!ljgj1x6s$6+0xa8(9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['ksfastners.herokuapp.com' , '127.0.0.1' , 'ksmain-2.herokuapp.com']
+ALLOWED_HOSTS = ['ksfastners.herokuapp.com' , '127.0.0.1' , 'ksmain-2.herokuapp.com' , 'ksfastners1.herokuapp.com']
 
 
 # Application definition
